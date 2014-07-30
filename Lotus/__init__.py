e7da930966a8637d2266334f43c6c35225b035df
Jayin Ton <tonjayin@gmail.com>
2014-07-30 21:29:45 +0800

    change the file structure

---
+++ Lotus/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+#import every thing here
+from .app import app
+from model import common
--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from app import app
-from config import default_config
-from sqlalchemy import create_engine
-from model import common
+from Lotus import app
+from Lotus.config import default_config
+
 
 if __name__ == '__main__':
     app.config.from_object(default_config.DevelopmentConfig)
@@ -11,6 +10,6 @@ if __name__ == '__main__':
     PORT = app.config.get('PORT')
     DEBUG = app.config.get('DEBUG')
     app.run(host=HOST, port=PORT, debug=DEBUG)
-#     print app.config['SQLALCHEMY_DATABASE_URI']
-#     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
-#     print engine.execute('select * from test').first()
+    # print app.config['SQLALCHEMY_DATABASE_URI']
+    # engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
+    # print engine.execute('select * from test').first()
